57fdfb5f0ce1403ac7811e2040af761d376f434a
shmykovandrey <shmykovandrey88@gmail.com>
2022-09-06 20:48:13 +0300

    complite 2015_04

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-const data = require('./mochData.js')
+const string = require('./mochData')
+const md5 = require('md5');
 
-let currentX = 0;
-let currentY = 0;
-
-let santaCurrentX = 0;
-let santaCurrentY = 0;
-
-let roboSantaCurrentX = 0;
-let roboSantaCurrentY = 0;
-
-let santaPath = []
-
-santaPath.push([currentX, currentY])
-
-data.split('').forEach((elem, i)=>{
-if (i % 2 === 1){
-  if (elem === '>') santaCurrentX++;
-  if (elem === '<') santaCurrentX--;
-  if (elem === '^') santaCurrentY++;
-  if (elem === 'v') santaCurrentY--;
-  if(!arrHasElem([santaCurrentX, santaCurrentY])) santaPath.push([santaCurrentX, santaCurrentY])
-}
-
-if (i % 2 === 0){
-  if (elem === '>') roboSantaCurrentX++;
-  if (elem === '<') roboSantaCurrentX--;
-  if (elem === '^') roboSantaCurrentY++;
-  if (elem === 'v') roboSantaCurrentY--;
-  if(!arrHasElem([roboSantaCurrentX, roboSantaCurrentY])) santaPath.push([roboSantaCurrentX, roboSantaCurrentY])
-}
-})
-
-function arrHasElem(elem){
-  for(let i=0; i < santaPath.length; i++){
-    if (santaPath[i][0] === elem[0] && santaPath[i][1] === elem[1]) return true
+for(let i = 0; true; i++){
+  let res = md5(string + i);
+  // console.log(res.slice(0,5));
+  if (res.slice(0,5)==='000000') {
+    console.log(i)
+    break;
   }
-  return false;
 }
-
-console.log(santaPath.length)
