2f3e157b956cec00e2ee020585ece17e4a3652b4
shmykovandrey <shmykovandrey88@gmail.com>
2022-09-12 00:49:17 +0300

    complite 2015_08 part1

--- index.js
+++ index.js
@@ -1,116 +1,43 @@
-///2015_07
-mochData = require("./mochDataTest");
+///2015_08
 mochData = require("./mochData");
+mochData = require("./mochDataTest");
+const fs = require("fs");
 
 //PART1
 
 function readMoch(mochData) {
-  dataArr = mochData.split("\n");
-  operandsArr = dataArr.map((elem) => elem.split(" -> "));
-  operandsArr = operandsArr.map((elem) => {
-    if (elem[0].indexOf("AND") !== -1)
-      return ["AND", elem[0].split(" AND "), elem[1]];
-    if (elem[0].indexOf("NOT") !== -1)
-      return ["NOT", elem[0].replace("NOT ", ""), elem[1]];
-    if (elem[0].indexOf("LSHIFT") !== -1)
-      return ["LSHIFT", elem[0].split(" LSHIFT "), elem[1]];
-    if (elem[0].indexOf("RSHIFT") !== -1)
-      return ["RSHIFT", elem[0].split(" RSHIFT "), elem[1]];
-    if (elem[0].indexOf("OR") !== -1)
-      return ["OR", elem[0].split(" OR "), elem[1]];
-    return ["=", elem[0], elem[1]];
-  });
-  return operandsArr;
-}
-
-function isNumber(num) {
-  if (num === "0" || num === 0) return true;
-  if (isNaN(+num)) return false;
-  if (!!num) return true;
-  return false;
-}
-
-function workWithData(parsedData) {
-  parsedData.forEach((elem) => {
-    // console.log(elem);
-    let current = null;
-    /// if =
-    if (elem[0] === "=") {
-      if (isNumber(elem[1])) current = +elem[1];
-      if (isNumber(allWise[elem[1]])) current = +allWise[elem[1]];
-      allWise[elem[2]] = current;
-    }
-    //// if NOT
-    if (elem[0] === "NOT") {
-      if (isNumber(elem[1])) current = elem[1];
-      if (isNumber(allWise[elem[1]])) current = allWise[elem[1]];
-      if (isNumber(current))
-        allWise[elem[2]] = +(
-          "0b" +
-          ("0".repeat(16 - current.toString(2).length) + current.toString(2))
-            .split("")
-            .map((el) => (el === "0" ? "1" : "0"))
-            .join("")
-        );
-    }
-    //// if AND
-    if (elem[0] === "AND") {
-      let current1 = null;
-      let current2 = null;
-      if (isNumber(elem[1][0])) current1 = elem[1][0];
-      if (isNumber(elem[1][1])) current2 = elem[1][1];
-      if (isNumber(allWise[elem[1][0]])) current1 = allWise[elem[1][0]];
-      if (isNumber(allWise[elem[1][1]])) current2 = allWise[elem[1][1]];
-      if (isNumber(current1) && isNumber(current2))
-        allWise[elem[2]] = current1 & current2;
-    }
-    if (elem[0] === "OR") {
-      let current1 = null;
-      let current2 = null;
-      if (isNumber(elem[1][0])) current1 = elem[1][0];
-      if (isNumber(elem[1][1])) current2 = elem[1][1];
-      if (isNumber(allWise[elem[1][0]])) current1 = allWise[elem[1][0]];
-      if (isNumber(allWise[elem[1][1]])) current2 = allWise[elem[1][1]];
-      if (isNumber(current1) && isNumber(current2))
-        allWise[elem[2]] = current1 | current2;
-    }
-    if (elem[0] === "LSHIFT") {
-      if (isNumber(elem[1][0])) current = elem[1][0];
-      if (isNumber(allWise[elem[1][0]])) current = allWise[elem[1][0]];
-      if (isNumber(current)) allWise[elem[2]] = current << +elem[1][1];
-    }
-    if (elem[0] === "RSHIFT") {
-      if (isNumber(elem[1][0])) current = elem[1][0];
-      if (isNumber(allWise[elem[1][0]])) current = allWise[elem[1][0]];
-      if (isNumber(current)) allWise[elem[2]] = current >> +elem[1][1];
-    }
-  });
-}
-
-function inicializeAllWise(parsedData) {
-  parsedData.forEach((elem) => (allWise[elem[2]] = null));
-}
-
-function haveNoNull(obj) {
-  let flag = true;
-  Object.keys(obj).forEach((elem) => {
-    if (obj[elem] === null) flag = false;
-  });
-  return flag;
-}
-//read moch data
-let parsedData = readMoch(mochData);
-const allWise = {};
-
-inicializeAllWise(parsedData);
-
-while (!haveNoNull(allWise)) {
-  workWithData(parsedData);
-
-  let count = 0;
-  for (key in allWise) {
-    if (allWise[key] !== null) count++;
+  try {
+    const data = fs.readFileSync("input.txt", "utf8");
+    // console.log(data.split("\n"));
+    return data.split("\n");
+  } catch (err) {
+    console.error(err);
   }
-  console.log(count);
 }
-console.log(allWise.a);
+
+let allLines = readMoch(mochData);
+
+let allSymbols = allLines.reduce((acc, elem) => (acc += elem.length), 0);
+
+console.log("all symbols " + allSymbols);
+let fullLines = allLines.map((elem) => elem);
+
+allLines = allLines.map((elem) => elem.replace(/\\x[a-f|0-9][a-f|0-9]/g, "1"));
+
+allLines = allLines.map((elem) => elem.replace(/\\./g, "1"));
+
+allLines = allLines.map((elem) => elem.replace(/"/g, ""));
+
+let allJSSymbol = allLines.reduce((acc, elem) => (acc += elem.length), 0);
+
+for (let i = 0; i < allLines.length - 1; i++) {
+  console.log(
+    fullLines[i],
+    fullLines[i].length,
+    allLines[i],
+    allLines[i].length
+  );
+}
+console.log(allJSSymbol);
+
+console.log(allSymbols - allJSSymbol);
--- mochDataTest.js
+++ mochDataTest.js
@@ -1,10 +1,6 @@
 //secret input
 
-module.exports = `123 -> x
-0 -> y
-x AND y -> d
-x OR y -> e
-x LSHIFT 2 -> f
-y RSHIFT 2 -> g
-NOT x -> h
-NOT 1 -> i`;
+module.exports = `""
+"abc"
+"aaa\"aaa"
+"\x27"`;
